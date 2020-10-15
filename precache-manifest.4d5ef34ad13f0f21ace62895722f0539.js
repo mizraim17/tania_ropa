641e5df079095195c83c8fcd54491df1e5dc74f2
kaneki <mizraim.martinez@gmail.com>
2020-10-15 15:08:14 -0500

    Updates

---
+++ precache-manifest.4d5ef34ad13f0f21ace62895722f0539.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af19032173f70caea46673853fd5146",
+    "url": "/tania_ropa/index.html"
+  },
+  {
+    "revision": "3e128575538bec939847",
+    "url": "/tania_ropa/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "c80c084cbbb3582f88b4",
+    "url": "/tania_ropa/static/css/main.d421a2d7.chunk.css"
+  },
+  {
+    "revision": "3e128575538bec939847",
+    "url": "/tania_ropa/static/js/2.899fee66.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/tania_ropa/static/js/2.899fee66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c80c084cbbb3582f88b4",
+    "url": "/tania_ropa/static/js/main.1c98dc68.chunk.js"
+  },
+  {
+    "revision": "4d0290ab1175c41dcde1",
+    "url": "/tania_ropa/static/js/runtime-main.7bb1f450.js"
+  }
+]);
